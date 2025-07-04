35bbba163210b894bb9e85205e2de74f3d99a3b3
hari0702 <hari0702>
2025-07-05 02:03:01 +0530

    funct

--- webapp.py
+++ webapp.py
@@ -1,22 +1,22 @@
 import streamlit as st
 # This is a simple Streamlit app for managing a to-do list
-import modules.function
+import function
 
 path = "/home/hari/webd/streamlit/webapp/todos.txt"  # Path to the todo file
 
 def add_task():
     todo = st.session_state.get("new_task", "").strip()  # Get the new task from session state
-    modules.function.append_todo_file(path, todo)  # Append the new task to the file
+    function.append_todo_file(path, todo)  # Append the new task to the file
 
 st.title("To-Do List App")
 st.subheader("Manage your tasks efficiently")
-read_tasks = modules.function.read_todo_file(path)
+read_tasks = function.read_todo_file(path)
 
 for index, task in enumerate(read_tasks):
    checkbox = st.checkbox(task, key=task)
    if checkbox:
          read_tasks.pop(index)
-         modules.function.write_todo_file(path, read_tasks)  # Update the file after removing
+         function.write_todo_file(path, read_tasks)  # Update the file after removing
          del st.session_state[task]  # Remove the task from session state
          st.rerun()
     
